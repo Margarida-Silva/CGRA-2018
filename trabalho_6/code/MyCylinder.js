1f1457915612c2c8c72a8c44c4574673ad0be429
MargaridaRPSilva <up201606214@g.uporto.pt>
2018-04-27 17:29:41 +0100

    added tp6 files

---
+++ trabalho_6/code/MyCylinder.js
@@ -0,0 +1,80 @@
+/**
+ * MyCylinder
+ * @param gl {WebGLRenderingContext}
+ * @constructor
+ */
+
+class MyCylinder extends CGFobject {
+	constructor(scene, slices, stacks, withTop) {
+		super(scene);
+		this.slices = slices;
+		this.stacks = stacks;
+		this.top = new MyCylinderTop(scene, slices);
+		this.withTop = withTop;
+		this.initBuffers();
+	};
+
+	initBuffers() {
+		this.vertices = [];
+		this.indices = [];
+		this.normals = [];
+
+		let deg2rad = Math.PI / 180.0;
+		let sliceAngle = (360.0 / this.slices) * deg2rad;
+
+		for (let i = 0; i < this.slices; i++) {
+			let angle = i * sliceAngle;
+			let x = Math.cos(angle);
+			let y = Math.sin(angle);
+
+			let normalX = Math.cos(angle);
+			let normalY = Math.sin(angle);
+
+			this.vertices.push(x, y, 0);
+			this.normals.push(normalX, normalY, 0);
+
+			for (let j = 0; j < this.stacks; j++) {
+				this.vertices.push(x, y, (j + 1) * (1.0 / this.stacks));
+				this.normals.push(normalX, normalY, 0);
+
+				if (i > 0) {
+					let base1 = (this.stacks + 1) * (i - 1) + j;
+					let base2 = (this.stacks + 1) * i + j;
+					this.indices.push(base1 + 1, base1, base2 + 1);
+					this.indices.push(base2, base2 + 1, base1);
+				}
+
+				if (i == this.slices - 1) {
+					let base1 = (this.stacks + 1) * i + j;
+					let base2 = j //equivalent to i==0;
+					this.indices.push(base1 + 1, base1, base2 + 1);
+					this.indices.push(base2, base2 + 1, base1);
+				}
+			}
+		}
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+	};
+
+	display() {
+		this.drawElements(this.primitiveType);
+
+		if (this.withTop) {
+			////// cylinder's bottom and top //////
+
+			let deg2rad = Math.PI / 180.0;
+
+			//bottom
+			this.scene.pushMatrix();
+			this.scene.rotate(-180 * deg2rad, 1, 0, 0);
+			this.top.display();
+			this.scene.popMatrix();
+
+			//top
+			this.scene.pushMatrix();
+			this.scene.translate(0, 0, 1);
+			this.top.display();
+			this.scene.popMatrix();
+		}
+	}
+};
