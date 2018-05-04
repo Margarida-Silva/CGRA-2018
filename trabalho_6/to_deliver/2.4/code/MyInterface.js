b5990a0186a68ba6e970c2a29f0a36f9efa2c1e2
SofiaCardosoMartins <up201606033@g.uporto.pt>
2018-05-04 22:50:12 +0100

    Finished part 2 of project 6.

---
+++ trabalho_6/to_deliver/2.4/code/MyInterface.js
@@ -0,0 +1,66 @@
+class MyInterface extends CGFinterface {
+
+
+	/**
+	 * MyInterface
+	 * @constructor
+	 */
+ 	constructor () {
+ 		super();
+ 	}
+	
+	/**
+	 * init
+	 * @param {CGFapplication} application
+	 */
+	init(application) {
+		// call CGFinterface init
+		super.init(application);
+
+		// init GUI. For more information on the methods, check:
+		//  http://workshop.chromeexperiments.com/examples/gui
+
+		this.gui = new dat.GUI();
+
+		// add a button:
+		// the first parameter is the object that is being controlled (in this case the scene)
+		// the identifier 'doSomething' must be a function declared as part of that object (i.e. a member of the scene class)
+		// e.g. LightingScene.prototype.doSomething = function () { console.log("Doing something..."); }; 
+
+		this.gui.add(this.scene, 'drawAxis').name("Draw Axis");	
+
+		// add a group of controls (and open/expand by defult)
+
+		var group=this.gui.addFolder("Luzes");
+		group.open();
+
+		// add two check boxes to the group. The identifiers must be members variables of the scene initialized in scene.init as boolean
+		// e.g. this.option1=true; this.option2=false;
+
+		group.add(this.scene, 'light1');
+        group.add(this.scene, 'light2');
+        group.add(this.scene, 'light3');
+        group.add(this.scene, 'light4');
+
+		return true;
+	};
+
+	/**
+	 * processKeyboard
+	 * @param event {Event}
+	 */
+	processKeyboard(event) {
+		// call CGFinterface default code (omit if you want to override)
+		super.processKeyboard(event);
+
+		// Check key codes e.g. here: http://www.asciitable.com/
+		// or use String.fromCharCode(event.keyCode) to compare chars
+
+		// for better cross-browser support, you may also check suggestions on using event.which in http://www.w3schools.com/jsref/event_key_keycode.asp
+		switch (event.keyCode)
+		{
+			case (65):	// only works for capital 'A', as it is
+				console.log("Key 'A' pressed");
+		};
+	};
+};
