8f9d4d6d2c3c63ef7799da74fac0a0fcac06264a
MargaridaRPSilva <up201606214@g.uporto.pt>
2018-05-24 18:41:18 +0100

    crane improvements

--- trabalho_6/code/MyCrane.js
+++ trabalho_6/code/MyCrane.js
@@ -13,7 +13,7 @@ class MyCrane extends CGFobject {
 
         let deg2rad = Math.PI / 180.0;
         this.arm1angle = 20*deg2rad;
-        this.arm2angle = 0 * deg2rad;
+        this.arm2angle = -10 * deg2rad;
 
         //"Recolha"
         this.RX1 = 5;
@@ -44,53 +44,60 @@ class MyCrane extends CGFobject {
 
         this.scene.pushMatrix();
         this.scene.pushMatrix();
-        this.scene.translate(10, 0.5, -10);
+        this.scene.translate(10, 0, -10);
 
 
         this.scene.pushMatrix();
         this.scene.rotate(this.horizontalAngle, 0, 1, 0);
 
         //Base
+        /*
         this.scene.pushMatrix();
         this.scene.scale(0.5, 0.5, 0.5);
         this.scene.rotate(90*deg2rad, 1, 0, 0);
         this.base.display();
         this.scene.popMatrix();
+        */
+        
 
         //Everything execept the base will be rotated angle1 + arm1angle degrees
         this.scene.pushMatrix();
         this.scene.rotate(this.arm1angle, 1, 0, 0);
 
         //Arm1
+        
         this.scene.pushMatrix();
-        this.scene.translate(0, -0.2, 0);
         this.scene.rotate(-90*deg2rad, 1, 0, 0);
         this.arm.display();
         this.scene.popMatrix();
+        
 
         //Articulation
+        
         this.scene.pushMatrix();
         this.scene.scale(0.5, 0.5, 0.5);
         this.scene.rotate(90*deg2rad, 0, 1, 0);
-        this.scene.translate(0, 10, -0.5);
+        this.scene.translate(0, 10, 0);
         this.base.display();
         this.scene.popMatrix();
+        
+        
 
          //Arm2
+         
          this.scene.pushMatrix();
          this.scene.scale(1, 1, 0.7);
          this.scene.translate(0, 5, 0);
          this.scene.rotate(this.arm2angle, 1, 0, 0);
          this.arm.display();
          this.scene.popMatrix();
+         
 
          //end of rotation angle1 + arm1angle degrees
          this.scene.popMatrix();
 
-
          this.scene.pushMatrix();
-         this.scene.translate(0, -2, 0);
-        this.scene.translate(0 , (Math.cos(this.arm1angle))*5  - Math.sin(this.arm2angle)*5, Math.sin(this.arm1angle)*5 + Math.cos(this.arm2angle)*5*0.65);
+         this.scene.translate(0, Math.cos(this.arm1angle) * 5 - Math.sin(this.arm2angle +this.arm1angle )*5*0.7, Math.sin(this.arm1angle)*5 + Math.cos(this.arm2angle+this.arm1angle)*5*0.7);
         //console.log("arm1: " + this.arm1angle + "  arm2:" + this.arm2angle);
          this.magnet.display();
          this.scene.popMatrix();
@@ -100,11 +107,17 @@ class MyCrane extends CGFobject {
          // end of horizontal rotation
          this.scene.popMatrix();
 
+        
          this.scene.popMatrix();
+         
          this.scene.pushMatrix();
+         this.scene.translate(10, 0.5, -10);
          this.scene.rotate(this.horizontalAngle, 0, 1, 0);
-         if (this.vehicle != null)
+         this.scene.translate(-10, -0.5, 10);
+         if (this.vehicle != null){
+             console.log("displaying");
          this.vehicle.display();
+         }
 
          this.scene.popMatrix();
 
@@ -138,7 +151,7 @@ class MyCrane extends CGFobject {
     update(currTime, vehicle){
         if (vehicle.carLocation[0] >= this.RX1 && vehicle.carLocation[1] <= this.RX2 && vehicle.carLocation[0] >= this.RY1 && vehicle.carLocation[1]<= this.RY2){
         vehicle.isAttached = true;
-        this.vehicle = vehicle
+        this.vehicle = vehicle;
         }
         else this.vehicle = null;
         if (this.degreesLeft > 0){
@@ -149,6 +162,11 @@ class MyCrane extends CGFobject {
           
             this.degreesLeft -= this.angleDec;
             }
-            else this.operating = false;
+            else {
+                this.operating = false;
+                if (this.vehicle != null)
+                this.vehicle.isAttached = false;
+                this.vehicle = null;
+            }
     }
 };
--- trabalho_6/code/MyMagnet.js
+++ trabalho_6/code/MyMagnet.js
@@ -10,6 +10,9 @@ class MyMagnet extends CGFobject {
         let deg2rad = Math.PI / 180.0;
 
         this.scene.pushMatrix();
+        this.scene.translate(0, -2, 0);
+
+        this.scene.pushMatrix();
         this.scene.translate(0, 2, 0);
         this.scene.rotate(90 * deg2rad, 1, 0, 0);
         this.scene.scale(0.1, 0.1, 2);
@@ -17,8 +20,11 @@ class MyMagnet extends CGFobject {
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
+        //this.scene.translate(0, 0.5, 0);
         this.scene.rotate(90 * deg2rad, 1, 0, 0);
         this.magnetBlock.display();
+        this.scene.popMatrix();
+
         this.scene.popMatrix();
     }
 };
