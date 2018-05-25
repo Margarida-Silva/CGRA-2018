8794d0c1ad33fc06655d0f150d568dbcd09009a5
SofiaCardosoMartins <up201606033@g.uporto.pt>
2018-05-25 09:35:33 +0100

    Added textures to the crane

--- trabalho_6/code/MyCraneArm.js
+++ trabalho_6/code/MyCraneArm.js
@@ -7,19 +7,17 @@
 class MyCraneArm extends CGFobject {
 	constructor(scene) {
         super(scene);
-        this.arm = new MyPrism(scene, 4, 20, true);
+        this.arm = new MyCylinder(scene, 4, 20, true);
 	};
 
 	
 
 display() {
-
         //Arm
         this.scene.pushMatrix();
         this.scene.scale(0.3, 0.3, 5);
         this.arm.display();
         this.scene.popMatrix();
-
     }
 
 };
