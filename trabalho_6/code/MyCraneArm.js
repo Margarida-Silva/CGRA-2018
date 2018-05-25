1d23c25d1889449e9eece234c6fec0f438bf3a8c
MargaridaRPSilva <up201606214@g.uporto.pt>
2018-05-25 09:44:08 +0100

    last merge

--- trabalho_6/code/MyCraneArm.js
+++ trabalho_6/code/MyCraneArm.js
@@ -7,22 +7,12 @@
 class MyCraneArm extends CGFobject {
 	constructor(scene) {
         super(scene);
-<<<<<<< HEAD
-        this.arm = new MyPrism(scene, 4, 20, true);
-        this.thickness = 0.3;
-=======
         this.arm = new MyCylinder(scene, 4, 20, true);
->>>>>>> 8794d0c1ad33fc06655d0f150d568dbcd09009a5
+        this.thickness = 0.3;
 	};
 
-	
-
-<<<<<<< HEAD
 display(length) {
 
-=======
-display() {
->>>>>>> 8794d0c1ad33fc06655d0f150d568dbcd09009a5
         //Arm
         this.scene.pushMatrix();
         this.scene.scale(this.thickness, this.thickness, length);
