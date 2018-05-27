dc3e2e3336c553020808336726764057e907f085
SofiaCardosoMartins <up201606033@g.uporto.pt>
2018-05-27 12:13:46 +0100

    Javadoc: MyUnitCubeQuad.js

--- trabalho_6/code/MyTerrain.js
+++ trabalho_6/code/MyTerrain.js
@@ -7,15 +7,15 @@ class MyTerrain extends Plane {
 	* @param {Array} altimetry A multidimensonal array representing the z values of each vertice in the plane
  	* @constructor
  	*/
-    constructor(scene, nrDivs,altimetry) {
-        super(scene, 0, 1, 0, 1, nrDivs,altimetry);
+    constructor(scene, nrDivs, altimetry) {
+        super(scene, 0, 1, 0, 1, nrDivs, altimetry);
 
         this.appearance = new CGFappearance(scene);
         this.appearance.setSpecular(0.1, 0.1, 0.1, 1);
         this.appearance.setShininess(0.1);
         this.appearance.setDiffuse(0.9, 0.9, 0.9, 1);
         this.appearance.loadTexture("../resources/images/mountain2.png");
-        this.appearance.setTextureWrap('CLAMP_TO_EDGE','CLAMP_TO_EDGE');
+        this.appearance.setTextureWrap('CLAMP_TO_EDGE', 'CLAMP_TO_EDGE');
     };
 
     /**
@@ -25,7 +25,7 @@ class MyTerrain extends Plane {
         let deg2rad = Math.PI / 180.0;
 
         this.scene.pushMatrix();
-        this.scene.rotate(-90*deg2rad,1,0,0);
+        this.scene.rotate(-90 * deg2rad, 1, 0, 0);
         this.scene.scale(100, 100, 1);
         this.appearance.apply();
         this.drawElements(this.primitiveType);
